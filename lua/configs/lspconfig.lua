e792aa9db0bbf6f1690fb7431e22576f740fb87e
dem0ngo <matthewgoulding192@gmail.com>
2024-03-24 18:13:27 -0600

    Initial commit after changing NvChad's configuration

---
+++ lua/configs/lspconfig.lua
@@ -0,0 +1,25 @@
+-- EXAMPLE 
+local on_attach = require("nvchad.configs.lspconfig").on_attach
+local on_init = require("nvchad.configs.lspconfig").on_init
+local capabilities = require("nvchad.configs.lspconfig").capabilities
+
+local lspconfig = require "lspconfig"
+-- Before these will be recognized make sure to add them to
+-- the mason setup in plugins/init.lua under "ensure_installed"
+local servers = { "html", "cssls", "marksman", "pyright" }
+
+-- lsps with default config
+for _, lsp in ipairs(servers) do
+  lspconfig[lsp].setup {
+    on_attach = on_attach,
+    on_init = on_init,
+    capabilities = capabilities,
+  }
+end
+
+-- typescript
+lspconfig.tsserver.setup {
+  on_attach = on_attach,
+  on_init = on_init,
+  capabilities = capabilities,
+}
